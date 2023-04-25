e9112ce7f748881a990343b8dd2c3f293caa1dc9
GalvinGao <me@galvingao.com>
2023-04-25 17:04:57 -0400

    feat: tweak with the animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import mdiGithub from "@iconify/icons-mdi/github";
 import { Icon } from "@iconify/react";
+import { motion } from "framer-motion";
 import { ReactNode } from "react";
 import "./App.css";
 
@@ -7,10 +8,38 @@ function NoLineBreak({ children }: { children: ReactNode }) {
   return <span className="whitespace-nowrap">{children}</span>;
 }
 
+function SectionWithMotion({
+  children,
+  ...props
+}: {
+  children: ReactNode;
+  [key: string]: any;
+}) {
+  return (
+    <motion.section
+      initial={{ opacity: 0, y: 40, scale: 0.8 }}
+      animate={{ opacity: 1, y: 0, scale: 1 }}
+      transition={{
+        type: "spring",
+        delay: props.delay || 0,
+        duration: 1.2,
+        bounce: 0.3,
+      }}
+      {...props}
+    >
+      {children}
+    </motion.section>
+  );
+}
+
 function App() {
   return (
     <main className="flex flex-col items-center justify-center py-2 h-full max-w-2xl mx-auto gap-4">
-      <section aria-label="Header" className="flex flex-col items-center">
+      <SectionWithMotion
+        aria-label="Header"
+        className="flex flex-col items-center"
+        delay={0.1}
+      >
         <h1 className="font-bold text-[calc(4vw+4vh+10px)] leading-tight text-gray-950 dark:text-gray-50">
           ArkAPI
         </h1>
@@ -31,13 +60,14 @@ function App() {
             API 服务<span className="-mr-[calc(0.2vw)]">。</span>
           </NoLineBreak>
         </p>
-      </section>
+      </SectionWithMotion>
 
       <div className="mt-[calc(3vw+3vh+10px)]" />
 
-      <section
+      <SectionWithMotion
         className="flex flex-col items-center justify-center gap-2"
         aria-label="Links"
+        delay={0.3}
       >
         <a
           href="https://github.com/ArkAlliance/ArkAPI"
@@ -47,13 +77,17 @@ function App() {
         >
           <Icon icon={mdiGithub} className="w-8 h-8 md:w-12 md:h-12" />
         </a>
-      </section>
+      </SectionWithMotion>
 
-      <footer className="flex flex-col items-center justify-center gap-2">
+      <SectionWithMotion
+        className="flex flex-col items-center justify-center gap-2"
+        aria-label="Footer"
+        delay={0.35}
+      >
         <span className="text-sm text-gray-500 dark:text-gray-400">
           Made with ❤️ by ArkAlliance.
         </span>
-      </footer>
+      </SectionWithMotion>
     </main>
   );
 }
